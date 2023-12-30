2a2e3adeb3811f37c1d8ea23a21313181c74ec4e
Anssi Lehtelä <anssi.lehtela@sok.fi>
2023-12-30 20:47:33 +0200

    reorg a bit

--- test_12.py
+++ test_12.py
@@ -8,6 +8,36 @@ example_input = """???.### 1,1,3
 ????.######..#####. 1,6,5
 ?###???????? 3,2,1"""
 
+def solve_puzzle_1(input):
+    lines = input.splitlines()
+    sum = 0
+    for line in lines:
+        sum += condition_variations(line)
+
+    return sum
+
+def condition_variations(input):
+    condition_records_input, contiguous_group_input = input.split(' ')
+    contiguous_group_input = [int(x) for x in contiguous_group_input.split(',')]
+
+    variations = create_variations(condition_records_input)
+    condition_records = []
+    for val in variations:
+        condition_records.append(condition_record(val))
+
+    return condition_records.count(contiguous_group_input)
+
+
+def create_variations(input):
+    variations = []
+    q = input.count('?')
+    for item in itertools.product(['.','#'], repeat = q):
+        new = input
+        for val in item:
+            new = new.replace('?', val, 1)
+        variations.append(new)
+    
+    return variations
 
 def condition_record(input):
     count = 0
@@ -29,18 +59,6 @@ def test_condition_record():
 
     assert condition_record(input) == [1,1,3]
 
-def create_variations(input):
-    variations = []
-    q = input.count('?')
-    for item in itertools.product(['.','#'], repeat = q):
-        new = input
-        for val in item:
-            new = new.replace('?', val, 1)
-        variations.append(new)
-    
-    return variations
-
-
 def test_create_variations():
     input = '???.###'
 
@@ -49,32 +67,12 @@ def test_create_variations():
     assert len(variations) == 8
     assert '###.###' in variations
 
-def condition_variations(input):
-    condition_records_input, contiguous_group_input = input.split(' ')
-    contiguous_group_input = [int(x) for x in contiguous_group_input.split(',')]
-
-    variations = create_variations(condition_records_input)
-    condition_records = []
-    for val in variations:
-        condition_records.append(condition_record(val))
-
-    return condition_records.count(contiguous_group_input)
-
-
 def test_condition_variations():
     input = '???.### 1,1,3'
     input2 = '.??..??...?##. 1,1,3'
 
     assert condition_variations(input) == 1
     assert condition_variations(input2) == 4
-
-def solve_puzzle_1(input):
-    lines = input.splitlines()
-    sum = 0
-    for line in lines:
-        sum += condition_variations(line)
-
-    return sum
 
 def test_solve_puzzle_1(day12_input):
     assert solve_puzzle_1(example_input) == 21
